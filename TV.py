6301c7c53e043076dd032eeaa3094e9871f2412d
Aslngsng <salangsang.abril@gmail.com>
2024-05-23 03:03:58 +0800

    added pseudocode

---
+++ TV.py
@@ -0,0 +1,9 @@
+# create a method for turning the TV on/off. 
+# create a method for setting the channel.
+# create a method for the volume level of the TV.
+# create another file for the TV Test named "TestTV" to call the function on TV.
+
+class TV():
+    def __init__(self) -> None:
+        self.on = False
+    def turn_on(self):
