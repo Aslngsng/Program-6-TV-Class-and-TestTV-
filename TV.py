88ab54c7b8e1573f6b63705a55e56a0d5725f363
Aslngsng <salangsang.abril@gmail.com>
2024-05-24 02:03:14 +0800

    fix the on/off method

--- TV.py
+++ TV.py
@@ -4,9 +4,12 @@
 # create another file for the TV Test named "TestTV" to call the function on TV.
 
 class TV():
-    def __init__(self) -> None:
+    def __init__(self, number_of_tv: str) -> None:
         self.on = False
+        self.state = number_of_tv
     def turn_on(self):
         self.on = True
     def turn_off(self):
         self.on = False
+    def display_state(self):
+        state = "on" if self.on else "off"
--- TestTV.py
+++ TestTV.py
@@ -1,13 +1,5 @@
 from TV import TV
-tv1 = TV()
+tv1 = TV("tv1")
 tv1.turn_on()
-if tv1.on: 
-    print("On")
-else:
-    print("off")
-tv2 = TV()
+tv2 = TV("tv2")
 tv2.turn_on()
-if tv2.on: 
-    print("On")
-else:
-    print("off")
