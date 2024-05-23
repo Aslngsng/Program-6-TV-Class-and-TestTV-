c93abb3a1d1c2f89173918244ca7678719db52f5
Aslngsng <salangsang.abril@gmail.com>
2024-05-23 22:35:57 +0800

    added the method on/off

--- TV.py
+++ TV.py
@@ -7,3 +7,18 @@ class TV():
     def __init__(self) -> None:
         self.on = False
     def turn_on(self):
+        self.on = True
+    def turn_off(self):
+        self.on = False
+tv1 = TV()
+tv1.turn_on()
+if tv1.on: 
+    print("On")
+else:
+    print("off")
+tv2 = TV()
+tv2.turn_on()
+if tv2.on: 
+    print("On")
+else:
+    print("off")
