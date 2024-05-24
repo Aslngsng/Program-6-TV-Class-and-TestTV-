07898ee371aba7a508db5542760e4d389b491abe
Aslngsng <159881658+Aslngsng@users.noreply.github.com>
2024-05-24 15:47:09 +0800

    Update TestTV.py

--- TestTV.py
+++ TestTV.py
@@ -1,13 +1,17 @@
+# Import the TV class from the “TV”
 from TV import TV
 
+# Create an instance of TV named 'tv1' with the identifier 'tv1'
 tv1 = TV("tv1")
 tv1.turn_on()
 tv1.prefer_channel(30)
 tv1.set_volume(3)
+# Create another instance of TV named 'tv2' with the identifier 'tv2'
 tv2 = TV("tv2")
 tv2.turn_on()
 tv2.prefer_channel(3)
 tv2.set_volume(2)
 
+# Display the settings of tv1 and tv2
 tv1.display_settings()
 tv2.display_settings()
